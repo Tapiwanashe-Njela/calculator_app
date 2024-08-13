cdc387e518da7cbfbd38bf468a5aceca5d7d0b6c
tsn-work-laptop <seannjela@gmail.com>
2024-08-13 13:03:03 -0500

    added styling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Import modules
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QGridLayout
+from PyQt5.QtGui import QFont
 
 class CalcApp(QWidget):
     def __init__(self):
@@ -12,6 +13,7 @@ class CalcApp(QWidget):
 
         #Create all app objects(widgets)
         self.text_box = QLineEdit() #Create a linear text box
+        self.text_box.setFont(QFont("Helvetica", 32)) #set font to Helvetica with size 32
         self.grid = QGridLayout() #Create a self.grid object
 
         self.button_list = ["7", "8", "9", "/", 
@@ -27,8 +29,10 @@ class CalcApp(QWidget):
         for text in self.button_list:
             button = QPushButton(text) #Create a button with text = respective text in list
             button.clicked.connect(self.button_click) #Create a button click event for the created button that connects to a function
+            button.setStyleSheet("QPushButton { font: 25px Comic Sans MS; padding: 10px; }") #allows us to use CSS in PyQt
             self.grid.addWidget(button, row, column) #Add the creted button to the self.grid
             column += 1 #To move to the next space on the right 
+
             if column > 3: #If we reach the end of the column 
                 row += 1 #We jump to the next row 
                 column = 0 #And we start again from the first column
@@ -44,6 +48,7 @@ class CalcApp(QWidget):
         master_layout.addWidget(self.text_box)
         master_layout.addLayout(self.grid)
         master_layout.addLayout(bottom_button_row)
+        master_layout.setContentsMargins(25,25,25,25) #small spacing inbetween our buttons right,left,top,bottom
 
         bottom_button_row.addWidget(self.clear_button)
         bottom_button_row.addWidget(self.delete_button)
@@ -67,7 +72,7 @@ class CalcApp(QWidget):
             #now we try to evaluate the obtained text from the text box
             try: #The try block lets you test a block of code for errors.
                 result = eval(text_box_text) #eval method evaluates the input string to make sure it is valid maths + avoid errors like divide by 0
-                self.text_box.setText("Result: " + str(result)) #If eval passes we display the value of result in our textbox after we convert it to a string
+                self.text_box.setText(str(result)) #If eval passes we display the value of result in our textbox after we convert it to a string
             
             except Exception as e: #The except block lets you handle the error.; using keyword Exception means its a broad catch and we give it a name e 
                 print("Error!:", e) #Print the type of error  
@@ -88,7 +93,8 @@ class CalcApp(QWidget):
 if __name__ == "__main__":
     app = QApplication([]) #Allows us to create and execute our app; takes in an empty list ALWAYS
     main_window = CalcApp() #Object to create a new form (window) that we will be editing
+    main_window.setStyleSheet("QWidget { background-color: #123 }") #add css styling to the main window
     main_window.show() #Display the form
     app.exec_() #Run the app
 
-#current --> https://youtu.be/f_9NBdSAo-g?t=6470
+#current --> https://youtu.be/f_9NBdSAo-g?t=6748
